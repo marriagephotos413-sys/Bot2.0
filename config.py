d613cd427cc231650f4e675ded03ed5f3da07e4a
marriagephotos413-sys <marriagephotos413@gmail.com>
2026-08-18 13:55:20 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = "YOUR_API_ID"       # Apni Telegram API ID dalein
+API_HASH = "YOUR_API_HASH"   # Apna Telegram API Hash dalein
+BOT_TOKEN = "YOUR_BOT_TOKEN" # Apna Bot Token dalein
+MONGO_URI = "YOUR_MONGO_URI" # Apni MongoDB ka URL dalein
+ADMIN_USER_ID = 123456789    # Apni Telegram Admin ID dalein
